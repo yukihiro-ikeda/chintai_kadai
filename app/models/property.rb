da3c0df9f6c5df02141e03c559d1d9e02e6bf3c5
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-27 15:47:19 +0900

    最寄駅が0もしくは１でも物件を登録できるように修正

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
   has_many :stations, dependent: :destroy
-  accepts_nested_attributes_for :stations, allow_destroy: true
+  accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
 end
